88889d420a401f132f78c4dce2fafcb48f91508e
Elia Folisi <75161469+Hiworu@users.noreply.github.com>
2024-06-17 18:13:31 +0200

    Add files via upload

---
+++ ICD-10-code-F44/projectData/HomerVars.js
@@ -0,0 +1,113 @@
+/**
+* javascript variables for the Homer project:
+* Elia Folisi
+* Homer - The Story Flow Editor.
+* Copyright (c)2021-2024. Open Lab s.r.l - Florence, Italy
+* Developer: Pupunzi (Matteo Bicocchi)
+*
+**/
+
+class HomerVars {
+
+      static badEnde = false;
+      static badEnd = false;
+      static cabinet = false;
+      static bathtub = false;
+      static sink = false;
+      static bathroom = false;
+      static desk = false;
+      static trashBath = false;
+      static trashBed = false;
+      static trashDesk = false;
+      static floor = false;
+      static closet = false;
+      static bottom = false;
+      static top = false;
+      static browsing = false;
+      static sketchbook = false;
+      static cleaning = false;
+      static knife = false;
+      static apples = false;
+
+};
+
+class HomerActors {
+
+      static YOU = "You";
+
+}
+
+class HomerMeta {
+      static FLOW_STATE = {
+          IDEA : 'IDEA',
+          NOTES : 'NOTES',
+          DRAFT : 'DRAFT',
+          EDITOR : 'EDITOR',
+          FINAL : 'FINAL',
+      };
+      static MOOD = {
+          NEUTRAL : 'NEUTRAL',
+          HAPPY : 'HAPPY',
+          VERY_HAPPY : 'VERY_HAPPY',
+          SAD : 'SAD',
+          ANGRY : 'ANGRY',
+          AMAZED : 'AMAZED',
+      };
+      static EXPRESSION = {
+          RELAXED : 'RELAXED',
+          GRINNING : 'GRINNING',
+          PENSIVE : 'PENSIVE',
+          FROWNING : 'FROWNING',
+          CRYING : 'CRYING',
+          ENRAGED : 'ENRAGED',
+      };
+      static BALLOON_TYPE = {
+          SPEECH : 'SPEECH',
+          WHISPER : 'WHISPER',
+          THOUGHT : 'THOUGHT',
+          SCREAM : 'SCREAM',
+          SLEEP : 'SLEEP',
+          SING_SONG : 'SING_SONG',
+      };
+      static LOCATION = {
+          HOME : 'HOME',
+          OUTSIDE : 'OUTSIDE',
+          OFFICE : 'OFFICE',
+          PARK : 'PARK',
+          SEA : 'SEA',
+          MOUNTAIN : 'MOUNTAIN',
+      };
+      static CAMERA_DIRECTION = {
+          CLOSE_UP : 'CLOSE_UP',
+          ANGLE_ON : 'ANGLE_ON',
+          OFF_SCREEN : 'OFF_SCREEN',
+          FADE_OUT : 'FADE_OUT',
+          FADE_IN : 'FADE_IN',
+          VOICE_OVER : 'VOICE_OVER',
+      };
+      static LIGHTNING = {
+          KEY : 'KEY',
+          BACK : 'BACK',
+          PRACTICAL : 'PRACTICAL',
+          HARD : 'HARD',
+          SOFT : 'SOFT',
+          CHIAROSCURO : 'CHIAROSCURO',
+      };
+
+}
+
+class HomerLabels {
+
+
+}
+
+
+class HomerFlowSlugs {
+      static DAY_ONE = "Day one";
+      static DAY_TWO = "Day two";
+      static DAY_THREE = "Day three";
+      static SASI_UNICORNO = "Sasi Unicorno";
+      static DAY_FOUR = "Day four";
+      static DAY_FIVE = "Day five";
+ }
+
